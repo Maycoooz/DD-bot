52cecde820bebed78ce0a6191dfa2e11c8bdc72b
ODSTOSJ <39856685+ODSTOSJ@users.noreply.github.com>
2025-10-07 18:49:25 +0800

    Implement role seeding in main.py
    Added a function to seed default user roles into the database.

--- backend/main.py
+++ backend/main.py
@@ -6,8 +6,30 @@ from contextlib import asynccontextmanager
 from .database import create_db_and_tables
 from .api import routes, parent, admin, librarian
 
+from backend.database import create_db_and_tables, engine
+from backend.models import Roles, UserType
+from sqlmodel import Session, select
+
+def seed_roles():
+    default_roles = [
+        UserType.ADMIN,
+        UserType.PARENT,
+        UserType.LIBRARIAN,
+        UserType.KID
+    ]
+
+    with Session(engine) as session:
+        for role_enum in default_roles:
+            existing = session.exec(select(Roles).where(Roles.name == role_enum)).first()
+            if not existing:
+                session.add(Roles(name=role_enum))
+                print(f"✅ Added role: {role_enum}")
+        session.commit()
+        print("🎉 Roles seeded automatically!")
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    seed_roles()
     create_db_and_tables()
     yield
 
@@ -20,7 +42,8 @@ app.include_router(librarian.router)
 
 origins = [
     "http://localhost:5173",
-    "https://ddbot-ochre.vercel.app/"
+    "https://ddbot-ochre.vercel.app/",
+    "https://ddbot-ch6g.vercel.app",
 ]
 
 app.add_middleware(
