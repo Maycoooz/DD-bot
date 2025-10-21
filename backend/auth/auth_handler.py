00607754d91b888559f2c0681f811ceb8263fef5
ODSTOSJ <39856685+ODSTOSJ@users.noreply.github.com>
2025-10-21 18:48:18 +0800

    Update auth_handler.py

--- backend/auth/auth_handler.py
+++ backend/auth/auth_handler.py
@@ -1,7 +1,7 @@
 import jwt
 from fastapi import Depends, HTTPException, status, APIRouter
 from fastapi.security import OAuth2PasswordBearer
-from jwt import InvalidTokenError
+from jwt.exceptions import InvalidTokenError, ExpiredSignatureError
 from passlib.context import CryptContext
 from datetime import datetime, timedelta, timezone
 from sqlalchemy.orm import Session
