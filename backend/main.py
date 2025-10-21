52a537a5e4d7103df2ff6b6fa0b739f83792c5cf
ODSTOSJ <39856685+ODSTOSJ@users.noreply.github.com>
2025-10-21 18:36:35 +0800

    Added www.ddbot.site to CORS config

--- backend/main.py
+++ backend/main.py
@@ -27,6 +27,7 @@ app = FastAPI(
 origins = [
     "http://localhost:8000",
     "http://localhost:5173",
+    "https://www.ddbot.site",
     "https://ddbot-ch6g.vercel.app",
     "https://www.ddbot-ch6g.vercel.app",
     # 👇 Allow all Vercel preview deployments
