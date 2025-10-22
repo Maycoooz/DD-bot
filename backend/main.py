f56b19c73ab5696a80ec17f634e6741e7cd465c3
ODSTOSJ <39856685+ODSTOSJ@users.noreply.github.com>
2025-10-22 23:32:14 +0800

    Update CORS origins in main.py

--- backend/main.py
+++ backend/main.py
@@ -28,12 +28,9 @@ origins = [
     "http://localhost:8000",
     "http://localhost:5173",
     "https://www.ddbot.site",
+    "https://ddbot.site",
     "https://ddbot-ch6g.vercel.app",
-    "https://www.ddbot-ch6g.vercel.app",
-    # 👇 Allow all Vercel preview deployments
     "https://ddbot-ch6g-*vercel.app",
-    "https://ddbot-ch6g-*.vercel.app",
-    # 👇 Optional: allow all Vercel subdomains for your project
     "https://*.vercel.app",
     "https://www.youtube.com"
 ]
@@ -42,7 +39,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_methods=["*"],
     allow_headers=["*"],
 )
